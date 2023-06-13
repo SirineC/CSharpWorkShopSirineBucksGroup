ff4bde14a01c424f504c2aea5fd56e96eedca939
Sirine <91069476+SirineC@users.noreply.github.com>
2023-06-13 12:17:50 +0100

    Day 1: Creating Variables
    Practiced creating different types of variables and output them

---
+++ Day1PracticeCode/Day1PracticeCode/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Create a string variable
+
+string myName = "Sirine";
+
+//Creating another variable
+
+string superHeroName = "The Flash";
+string secondMovieName = "AdiPurush";
+
+//Console.WriteLine(superHeroName);
+
+//Building the output string
+//+ is the concatenation operator
+
+string outputString = "This Thursday, Jay will be watching the movie, " + superHeroName;
+
+Console.WriteLine(outputString);
+
+string outputString2 = "This Friday, Jay will be watching the movie, " + secondMovieName;
+
+Console.WriteLine(outputString2);
+
+//only characters use single quotes
+char firstLetter = 'a';
+//string testingmytheory = 'a';
+
+//Create a variable to store an integer
+
+int myAge = 18;
+
+//Console.WriteLine(myAge);
+
+//Create a decimal variable
+
+decimal myMoney = 100.50m;
+
+//Console.WriteLine(myMoney);
+
+//Create a double variable
+
+double myDouble = 100.50;
+
+//Console.WriteLine(myDouble);
+
+//Create a boolean variable
+
+bool isRaining = true;
+
+//Console.WriteLine(isRaining);
+
