7a13ba1f41cff0a179acc41afb26adc57a799bc9
Sirine <91069476+SirineC@users.noreply.github.com>
2023-06-15 12:12:15 +0100

    Day 1: Homework
    Homework for Day 1

---
+++ Day1PracticeCode/Day1Homework/Day1Homework/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//string variables
+string name = "Sirine";
+
+Console.WriteLine(name);
+
+string homework = "variables";
+
+Console.WriteLine(homework);
+
+//integer variables
+int age = 18;
+
+Console.WriteLine(age);
+
+int number = 1;
+
+Console.WriteLine(number);
+
+//double/decimal variables
+
+decimal money = 10.99m;
+
+Console.WriteLine(money);
+
+double price = 5.99;
+
+Console.WriteLine(price);
+
+//a string that combines at least 2 strings
+
+string sentence = name + " has £" + money;
+
+Console.WriteLine(sentence);
+
+
