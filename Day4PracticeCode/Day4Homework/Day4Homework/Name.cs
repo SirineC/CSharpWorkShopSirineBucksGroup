f018185c3e0991b821db1f766d0eca93eea93721
Sirine <91069476+SirineC@users.noreply.github.com>
2023-06-27 11:33:49 +0100

    Day 4: Homework
    Homework for Day 4

---
+++ Day4PracticeCode/Day4Homework/Day4Homework/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Homework
+{
+    //A simple class
+    public class Name
+    {
+        //class should have at least 5 properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool Adult { get; set; }
+        public int BirthYear { get; set; }
+
+    }
+}
---
+++ Day4PracticeCode/Day4Homework/Day4Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using Day4Homework;
+
+//then, use this class to create at least 3 objects in Program.cs
+
+Name firstperson = new Name();
+
+firstperson.FirstName = "Tom";
+firstperson.LastName = "Jones";
+firstperson.Age = 23;
+firstperson.Adult = true;
+firstperson.BirthYear = 2000;
+
+//build an output with the values stored in these 3 objects
+
+string firstpersoninfo = $"Name: {firstperson.FirstName} {firstperson.LastName} Age: {firstperson.Age} Birth Year: {firstperson.BirthYear} Adult: {firstperson.Adult}";
+
+Console.WriteLine(firstpersoninfo);
